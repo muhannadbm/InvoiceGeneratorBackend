74177ed7d64fd6f2a49e21c4c663e6c420909381
muhannadbm <mhd31145@hotmail.com>
2022-03-02 20:35:53 -0500

    trying tofix it

---
+++ migrations/20220303005630_firsttry.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema
+    .createTable('mynumbers', (table) => {
+      table.bigInteger('invoice_id')
+
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('mynumbers')
+};
