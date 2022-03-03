26719961fa5e31f0adc515b4e1da58fcb60c86f6
muhannadbm <mhd31145@hotmail.com>
2022-03-02 19:50:41 -0500

    changing router route for id

--- knexfile.js
+++ knexfile.js
@@ -3,62 +3,34 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
- const sharedConfig = {
-  client: 'postgresql',
-  useNullAsDefault: true,
-  migrations: {
-    directory: './data/migrations',
-  },
-  seeds: {
-    directory: './data/seeds',
-  },
-  // this enables foreign keys in SQLite
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done),
-      conn.run('PRAGMA journal_mode = "OFF"') 
-    },
-  },
-}
 module.exports = {
 
-  development: {
-  //   ...sharedConfig,
-  //   connection: {
-  //     filename: 'data/migrations/dev.sqlite3'
-  //   }
-  // },
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user:     'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-},
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+  development:{
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
+    migrations: {
+      directory: './migrations'
+    }
+  },
 
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations'
+    }
   },
 
   production: {
-    client: 'pq',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
